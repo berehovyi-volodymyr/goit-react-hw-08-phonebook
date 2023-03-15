a030374f45ea7c2b41c4ee3094aba35748efc22d
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2023-03-15 22:57:19 +0000

    Deploying to gh-pages from @ berehovyi-volodymyr/goit-react-hw-08-phonebook@c9069a08fd649e502bdc4fc2b510725a31e4db09 🚀

---
+++ static/js/77.fe4b1456.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[77],{5077:function(e,t,a){a.r(t),a.d(t,{default:function(){return c}});var n="homePage_title__FzVmN",r=a(184),c=function(){return(0,r.jsx)("h2",{className:n,children:"Do you want add contact? Let's start"})}}}]);
+//# sourceMappingURL=77.fe4b1456.chunk.js.map
